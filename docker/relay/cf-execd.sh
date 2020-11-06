ed144625156a94d23528d9f14460248411cbc0c7
Janos Mattyasovszky <mail@matya.hu>
2020-11-06 16:14:54 +0100

    Update cf-execd.sh
    Optimized some check logic to only happen if we are not generating the files ourselves
    Added folding logic to multi-lined files so they can be supplied as a single line, which is easier to manage

--- docker/relay/cf-execd.sh
+++ docker/relay/cf-execd.sh
@@ -15,37 +15,54 @@ fi
 
 uuid=$(cat /node_id/uuid.hive)
 
+VERIFY_PUBKEY=1
 if [ ! -f /agent_certs/ppkeys/localhost.priv ]; then
   mkdir -p /agent_certs/ppkeys
   if [ -n "$RUDDER_RELAY_RSA_KEY" ]; then
-    echo "$RUDDER_RELAY_RSA_KEY" > /agent_certs/ppkeys/localhost.priv
+    (
+      echo "-----BEGIN RSA PUBLIC KEY-----"
+      echo "$RUDDER_RELAY_RSA_KEY" | fold -w 64 
+      echo "-----END RSA PUBLIC KEY-----"
+    ) > /agent_certs/ppkeys/localhost.priv
     chmod 600 /agent_certs/ppkeys/localhost.priv
   else
     /opt/rudder/bin/cf-key -T 4096 -f /agent_certs/ppkeys/localhost
+    VERIFY_PUBKEY=0
   fi
 fi
 
 # Generate public key based on private key so we can verify those two match togehter
-openssl rsa -in /agent_certs/ppkeys/localhost.priv -RSAPublicKey_out > /tmp/verify.localhost.pub
-if [ ! -f /agent_certs/ppkeys/localhost.pub ]; then
-   mv /tmp/verify.localhost.pub /agent_certs/ppkeys/localhost.pub
-else
-   cmp /tmp/verify.localhost.pub /agent_certs/ppkeys/localhost.pub
+if (( VERIFY_PUBKEY == 1 )); then
+  openssl rsa -in /agent_certs/ppkeys/localhost.priv -RSAPublicKey_out > /tmp/verify.localhost.pub
+  if [ ! -f /agent_certs/ppkeys/localhost.pub ]; then
+     mv /tmp/verify.localhost.pub /agent_certs/ppkeys/localhost.pub
+  else
+    cmp /tmp/verify.localhost.pub /agent_certs/ppkeys/localhost.pub
+  fi
 fi
 
+VERIFY_CERT=1
 if [ ! -f /opt/rudder/etc/ssl/agent.cert ]; then
   mkdir -p /agent_certs/ssl
   if [ -n "$RUDDER_RELAY_CERTIFICATE" ]; then
-    echo "$RUDDER_RELAY_CERTIFICATE" > /opt/rudder/etc/ssl/agent.cert
+    (
+      echo "-----BEGIN CERTIFICATE-----"
+      echo "$RUDDER_RELAY_CERTIFICATE" | fold -w 64 
+      echo "-----END CERTIFICATE-----"
+    ) > /opt/rudder/etc/ssl/agent.cert
+
   else
+    VERIFY_CERT=0
     openssl req -new -sha256 -key /agent_certs/ppkeys/localhost.priv -out /agent_certs/ssl/agent.cert -passin "pass:Cfengine passphrase" -x509 -days 3650 -extensions agent_cert -config /opt/rudder/etc/ssl/openssl-agent.cnf -subj "/UID=${uuid}"
   fi
 fi
 
-# We verify that the certificate belongs to the private key (Modulus is identical)
-openssl x509 -noout -modulus -in /opt/rudder/etc/ssl/agent.cert > /tmp/verify.modulus.crt
-openssl rsa  -noout -modulus -in /agent_certs/ppkeys/localhost.priv > /tmp/verify.modulus.priv
-cmp /tmp/verify.modulus.crt /tmp/verify.modulus.priv
+if ((VERIFY_CERT == 1 )); then
+    # We verify that the certificate belongs to the private key (Modulus is identical)
+    openssl x509 -noout -modulus -in /opt/rudder/etc/ssl/agent.cert     > /tmp/verify.modulus.crt
+    openssl rsa  -noout -modulus -in /agent_certs/ppkeys/localhost.priv > /tmp/verify.modulus.priv
+    cmp /tmp/verify.modulus.crt /tmp/verify.modulus.priv
+fi
 
 rudder agent check -f
 
